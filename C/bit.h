1bb7cf7d9ed22a46c5f728391fe3d18280041c5d
Niket Naidu <coder137@gmail.com>
2020-08-03 10:34:56 -0700

    Update return statements on bit.h

--- C/bit.h
+++ C/bit.h
@@ -10,6 +10,7 @@
 /**
  * @brief Creates a UINT32 BITMASK of size n
  * @param n Numbers of bits used to create the BITMASK
+ * @return UINT32
  */
 #define BIT_MASK(n) BIT_MASK_HELPER(n, uint32_t)
 
@@ -50,6 +51,7 @@
 /**
  * @brief Creates a UINT64 BITMASK of size n
  * @param n Number of bits used to create the BITMASK
+ * @return UINT64
  */
 #define BIT_MASK64(n) BIT_MASK_HELPER(n, uint64_t)
 
@@ -61,7 +63,7 @@
  * @brief A generic BIT MASK API for different datatypes
  * @param pos: Shift by current position
  * @param type: Datatype used (uint32_t, uint64_t)
- * @return
+ * @return sizeof(type)
  */
 #define BIT_MASK_HELPER(pos, type) (~((type)(~0) << pos))
 
