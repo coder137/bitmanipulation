02de6de77d186262fa74c2fccf57e2c0eb1f6262
Niket Naidu <coder137@gmail.com>
2020-08-03 10:31:45 -0700

    Update bit.h with documentation

--- C/bit.h
+++ C/bit.h
@@ -3,15 +3,66 @@
 
 #include <stdint.h>
 
-#define BIT_MASK_HELPER(pos, type) (~((type)(~0) << pos))
+//
+// 32 BIT APIS
+//
 
-#define BIT_MASK(pos) BIT_MASK_HELPER(pos, uint32_t)
-#define BIT_MASK64(pos) BIT_MASK_HELPER(pos, uint64_t)
+/**
+ * @brief Creates a UINT32 BITMASK of size n
+ * @param n Numbers of bits used to create the BITMASK
+ */
+#define BIT_MASK(n) BIT_MASK_HELPER(n, uint32_t)
 
+/**
+ * @brief Set a BIT on the variable at a given position
+ * @param value The output variable that is modified
+ * @param pos The BIT position to set data (0 indexed)
+ */
 #define BIT_SET(value, pos) (value |= (1 << pos))
+
+/**
+ * @brief Set N BITS on the variable starting from a given position
+ * @param value The output variable that is modified
+ * @param start_pos The starting position of the bit (0 indexed)
+ * @param n The number of bits used
+ */
 #define BIT_SET_N(value, start_pos, n) (value |= (BIT_MASK(n) << start_pos))
 
+/**
+ * @brief Clear a BIT on the variable at a given position
+ * @param value This output variable that is modified
+ * @param pos The BIT position to clear data (0 indexed)
+ */
 #define BIT_RESET(value, pos) (value &= ~(1 << pos))
+
+/**
+ * @brief Clear N BITS on the variable starting from a given position
+ * @param value The output variable that is modified
+ * @param start_pos The starting position of the bit (0 indexed)
+ * @param n The number of bits used
+ */
 #define BIT_RESET_N(value, start_pos, n) (value &= ~(BIT_MASK(n) << start_pos))
+
+//
+// 64 BIT APIS
+//
+
+/**
+ * @brief Creates a UINT64 BITMASK of size n
+ * @param n Number of bits used to create the BITMASK
+ */
+#define BIT_MASK64(n) BIT_MASK_HELPER(n, uint64_t)
+
+//
+// HELPER APIS
+//
+
+/**
+ * @brief A generic BIT MASK API for different datatypes
+ * @param pos: Shift by current position
+ * @param type: Datatype used (uint32_t, uint64_t)
+ * @return
+ */
+#define BIT_MASK_HELPER(pos, type) (~((type)(~0) << pos))
 
 #endif
