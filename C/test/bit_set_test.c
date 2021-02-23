0f9a6242d3e73c6fc720a29c8b47803ed4072a6f
Niket Naidu <coder137@gmail.com>
2021-02-22 19:09:19 -0800

    Updated bit_set_test with tester APIs
    - Added boilerplate functions for inplace APIs
    - Updated README.md

--- C/test/bit_set_test.c
+++ C/test/bit_set_test.c
@@ -1,113 +1,134 @@
+#include "bit.h"
 #include "unity.h"
 
-#include "bit.h"
-
-static uint32_t set;
-
-void setUp() { set = 0; }
-void tearDown() {}
-
-void test_bit_set(void) {
-  uint32_t value = 0;
-
-  // Manual
-  value = BIT_SET_1(set, 0);
-  TEST_ASSERT_EQUAL_UINT32(value, 1);
-
-  value = BIT_SET_1(set, 1);
-  TEST_ASSERT_EQUAL_UINT32(value, 0x02);
-
-  value = BIT_SET_1(set, 2);
-  TEST_ASSERT_EQUAL_UINT32(value, 0x04);
-
-  value = BIT_SET_1(set, 3);
-  TEST_ASSERT_EQUAL_UINT32(value, 0x08);
-
-  value = BIT_SET_1(set, 4);
-  TEST_ASSERT_EQUAL_UINT32(value, 0x10);
-
-  // Automated
-  for (int i = 0; i < 32; i++) {
-    value = BIT_SET_1(set, i);
-    TEST_ASSERT_EQUAL_UINT32(value, (1 << i));
-  }
-
-  // Overflow will cause a -Wshift-count-overflow compile time error
-  // BIT_SET(set, 32);
-}
-
-static void test_bit_set_n(uint8_t start_pos, uint8_t n, uint32_t actual) {
-  uint32_t value = BIT_SET_N(set, start_pos, n);
+// Static functions
+static void tester_bit_set_1(uint8_t pos, uint32_t actual) {
+  uint32_t value = BIT_SET_1(0, pos);
   TEST_ASSERT_EQUAL_UINT32(value, actual);
 }
 
+static void tester_bit_set_n(uint8_t start_pos, uint8_t n, uint32_t actual) {
+  uint32_t value = BIT_SET_N(0, start_pos, n);
+  TEST_ASSERT_EQUAL_UINT32(value, actual);
+}
+
+void tester_bit_set_inplace_n(uint8_t start_pos, uint8_t n, uint32_t actual) {
+  uint32_t value = 0;
+  BIT_SET_INPLACE_N(value, start_pos, n);
+  TEST_ASSERT_EQUAL_UINT32(value, actual);
+}
+
+void setUp() {}
+void tearDown() {}
+
+// Set Bit 1
+void test_bit_set_1(void) {
+  tester_bit_set_1(0, 0x01);
+
+  tester_bit_set_1(1, 0x02);
+
+  tester_bit_set_1(2, 0x04);
+
+  tester_bit_set_1(3, 0x08);
+
+  tester_bit_set_1(4, 0x10);
+
+  // Automated
+  for (int i = 0; i < 32; i++) {
+    tester_bit_set_1(i, (1 << i));
+  }
+
+  // Overflow will cause a -Wshift-count-overflow compile time error
+  // NOTE, Only works on constants
+  // BIT_SET_1(0, 32);
+}
+
+// Set Bit N
+
 void test_bit_set_n_different_bit_mask(void) {
   // Check Manually
-  test_bit_set_n(0, 0, 0x00);
-  test_bit_set_n(0, 1, 0x01);
-  test_bit_set_n(0, 2, 0x03);
-  test_bit_set_n(0, 3, 0x07);
-  test_bit_set_n(0, 4, 0x0F);
+  tester_bit_set_n(0, 0, 0x00);
+  tester_bit_set_n(0, 1, 0x01);
+  tester_bit_set_n(0, 2, 0x03);
+  tester_bit_set_n(0, 3, 0x07);
+  tester_bit_set_n(0, 4, 0x0F);
 
   // Check automated
   for (int i = 0; i < 32; i++) {
-    test_bit_set_n(0, i, BIT_MASK(i));
+    tester_bit_set_n(0, i, BIT_MASK(i));
   }
 }
 
 void test_bit_set_n_different_start_position(void) {
   // Check Manually
-  test_bit_set_n(0, 1, 0x01);
-  test_bit_set_n(1, 1, 0x02);
-  test_bit_set_n(2, 1, 0x04);
-  test_bit_set_n(3, 1, 0x08);
+  tester_bit_set_n(0, 1, 0x01);
+  tester_bit_set_n(1, 1, 0x02);
+  tester_bit_set_n(2, 1, 0x04);
+  tester_bit_set_n(3, 1, 0x08);
 
   // Check automated
   for (int i = 0; i < 32; i++) {
-    test_bit_set_n(i, 1, (1 << i));
+    tester_bit_set_n(i, 1, (1 << i));
   }
 }
 
 void test_bit_set_n_different_bit_mask_and_start_position(void) {
   // Start from position 1 for n bits
-  test_bit_set_n(1, 2, 0b0110);
-  test_bit_set_n(1, 3, 0b1110);
-  test_bit_set_n(1, 4, 0b00011110);
-  test_bit_set_n(1, 5, 0b00111110);
-  test_bit_set_n(1, 6, 0b01111110);
-  test_bit_set_n(1, 7, 0b11111110);
+  tester_bit_set_n(1, 2, 0b0110);
+  tester_bit_set_n(1, 3, 0b1110);
+  tester_bit_set_n(1, 4, 0b00011110);
+  tester_bit_set_n(1, 5, 0b00111110);
+  tester_bit_set_n(1, 6, 0b01111110);
+  tester_bit_set_n(1, 7, 0b11111110);
 
   // Start from position 2 for n bits
-  test_bit_set_n(2, 2, 0b1100);
-  test_bit_set_n(2, 3, 0b00011100);
-  test_bit_set_n(2, 4, 0b00111100);
-  test_bit_set_n(2, 5, 0b01111100);
-  test_bit_set_n(2, 6, 0b11111100);
+  tester_bit_set_n(2, 2, 0b1100);
+  tester_bit_set_n(2, 3, 0b00011100);
+  tester_bit_set_n(2, 4, 0b00111100);
+  tester_bit_set_n(2, 5, 0b01111100);
+  tester_bit_set_n(2, 6, 0b11111100);
 
   // Start from position 3 for n bits
-  test_bit_set_n(3, 2, 0b00011000);
-  test_bit_set_n(3, 3, 0b00111000);
-  test_bit_set_n(3, 4, 0b01111000);
-  test_bit_set_n(3, 5, 0b11111000);
+  tester_bit_set_n(3, 2, 0b00011000);
+  tester_bit_set_n(3, 3, 0b00111000);
+  tester_bit_set_n(3, 4, 0b01111000);
+  tester_bit_set_n(3, 5, 0b11111000);
 
   // Start from position 4 for n bits
-  test_bit_set_n(4, 2, 0b00110000);
-  test_bit_set_n(4, 3, 0b01110000);
-  test_bit_set_n(4, 4, 0b11110000);
+  tester_bit_set_n(4, 2, 0b00110000);
+  tester_bit_set_n(4, 3, 0b01110000);
+  tester_bit_set_n(4, 4, 0b11110000);
 
   // Start from position 5 for n bits
-  test_bit_set_n(5, 2, 0b01100000);
-  test_bit_set_n(5, 3, 0b11100000);
+  tester_bit_set_n(5, 2, 0b01100000);
+  tester_bit_set_n(5, 3, 0b11100000);
 
   // Start from position 6 for n bits
-  test_bit_set_n(6, 2, 0b11000000);
+  tester_bit_set_n(6, 2, 0b11000000);
 }
+
+// Set Inplace 1
+void test_bit_inplace_1() {}
+
+// Set inplace n
+void test_bit_inplace_n() {}
 
 int main() {
   UNITY_BEGIN();
-  RUN_TEST(test_bit_set);
+
+  // Set 1
+  RUN_TEST(test_bit_set_1);
+
+  // Set n
   RUN_TEST(test_bit_set_n_different_bit_mask);
   RUN_TEST(test_bit_set_n_different_start_position);
   RUN_TEST(test_bit_set_n_different_bit_mask_and_start_position);
+
+  // Inplace 1
+  RUN_TEST(test_bit_inplace_1);
+
+  // Inplace n
+  RUN_TEST(test_bit_inplace_n);
+
   return UNITY_END();
 }
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 - [Bit Manipulation](#bit-manipulation)
 - [Usage](#usage)
+  - [Manual](#manual)
+  - [Use with CMake](#use-with-cmake)
 - [Build Process](#build-process)
   - [Clone the repository](#clone-the-repository)
   - [Generate and build library](#generate-and-build-library)
@@ -7,16 +9,38 @@
 
 # Bit Manipulation
 
-[![Build Status](https://travis-ci.org/coder137/bitmanipulation.svg?branch=master)](https://travis-ci.org/coder137/bitmanipulation)
+![Github Action](https://github.com/coder137/bitmanipulation/actions/workflows/cmake.yml/badge.svg)
 
 C and C++ Library for bit manipulation
 
 # Usage
 
-The **C** version of bit manipulation is just one header file and has one dependency
+The **C** version of bit manipulation has only header dependencies and has one standard library dependency
 
-- Copy `C/bit.h` to your project
-- Has dependency on `<stdint.h>` for data types
+## Manual
+
+- Copy `C/bit.h` and `C/include` to your project
+  - Make sure these are added to your include path
+- Has dependency on `<stdint.h>` for standard C data types
+
+## Use with CMake
+
+- Add this project as a submodule OR copy this project into your repository
+  - Make sure `Unity` has been added for unit testing as well
+- Use `add_subdirectory` to build the project with CMake
+  - Use the `bit` INTERFACE library with `target_link_libraries`
+
+```cmake
+add_subdirectory(bitmanipulation/C)
+
+add_executable(dummy_target
+  source_files.c
+)
+target_link_libraries(dummy_target
+  PRIVATE/PUBLIC
+  bit # The interface library to use
+)
+```
 
 # Build Process
 
